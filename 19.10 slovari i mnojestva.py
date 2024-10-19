433b1a49428ec02a9f10eeaa29f87022811a6210
Gasheck <kuksgauzenk9195711112@gmail.com>
2024-10-19 22:22:36 +0300

    решаю

--- 19.10 slovari i mnojestva.py
+++ 19.10 slovari i mnojestva.py
@@ -4,7 +4,17 @@ slovar[3] = 333
 slovar[4] = 4444  # добавил в словарь новую пару
 del slovar[1]
 print(type(slovar))
-slovar.update({5:555,
-               6:666})
-print(slovar.get(1, "такого ключа нет")) # возвращает значение по ключу, если ключа нет то НОН или текст после запятой
+
+slovar.update({5: 555,
+               6: 666})
+# key = int(input("введите искомый ключ - "))
+# print(slovar.get(key, "такого ключа нет"))
+# # возвращает значение по ключу, если ключа нет то НОН или текст после запятой
+# #key = int(input("введите удаляемый ключ - "))
+# print(slovar.pop(int(input("введите удаляемый ключ - ")), "такого ключа нет"))
+print(slovar.keys())
+print(slovar.values())
+print(slovar.items())
 print(slovar)
+set_=set(slovar)
+print(type(set_), "это уже множество")
