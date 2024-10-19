c5836b78bc5f390b6170ba5d57a3616d2ed83b44
Gasheck <kuksgauzenk9195711112@gmail.com>
2024-10-19 13:55:25 +0300

    novaya zadacha

---
+++ 19.10 list tuple.py
@@ -0,0 +1,5 @@
+tuple_=([1,2,3],1,2)
+print(tuple_)
+tuple_[0].remove(1)
+
+print(tuple_)
--- metodi strok 41.py
+++ metodi strok 41.py
@@ -1,7 +1,18 @@
 stroka='voina i mir'
 print(stroka.upper())
 print(stroka.lower())
-
+print(stroka[0].upper()+stroka[1:-1]+stroka[0].upper())
+print(stroka.islower())
+print(stroka[1].islower())
+print(stroka.isupper())
+print(stroka[1].isupper())
+print(stroka.replace('voina','xxx'))
+print(min(stroka))#не знаю что делает, точнее не понял
+print(stroka.partition('i'))
+print(id(stroka))
+izmeneniya=str.maketrans('voina ', '12345=')
+print(stroka.translate(izmeneniya))
+print(stroka)
 
 
 
