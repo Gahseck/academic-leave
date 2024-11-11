679732b8497db4c93638dcc8918f38150a4a076d
Gasheck <kuksgauzenk9195711112@gmail.com>
2024-11-11 12:33:37 +0300

    ошибка в крестиках-ноликах

---
+++ zadachi.py
@@ -0,0 +1,33 @@
+# k=6
+# n=7
+# for i in range(n):
+#     print(k)
+
+# a=3
+# b=10
+# sum=0
+# for i in range(a+1,b)[::-1]:
+#     sum+=1
+#     print(i)
+# print('sum - ', sum)
+
+# a = float(input("введите число A: "))
+# n = int(input("введите число N: "))
+# # multiplication_=1
+# # print(a**n)
+# # k=-1
+# for i in range(1,n+1):
+# #     # k*=-1
+# #     multiplication_ += 2*i-1
+#     p=(-1)**i * a**i
+#     print(p)
+
+# import random
+# def summ(a,b):
+#     res=a+b
+#     return res
+# er=summ(1.1,2)
+# print(er)
+# print(summ(6,9))
+rr=lambda x,y:x+y
+print(rr(1,9))
