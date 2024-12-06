978dcd317583395229b103d7862a5644750f1c5f
Gasheck <kuksgauzenk9195711112@gmail.com>
2024-12-06 14:17:33 +0500

    ошибка в крестиках-ноликах делю ходы

--- учу книгу пайтон.py
+++ учу книгу пайтон.py
@@ -1,31 +1,16 @@
-# задача 2-переставить элементы и посчитать длину списка
-# listing = []
-# while 1==1:
-#     element = input('Введите элемент, если список закончился введите "1" - ')
-#     if element != '1':
-#         listing.append(element)
-#     else: break
-# print(listing)
-# listing.reverse()
-# print(listing)
-# print('длина списка - ',len(listing))
-
-
-# задача 3. сортировка по возрастанию и убыванию
-dictionary={}
-while 1==1:
-    key=input('введите ключ, если выход из словаря введите "0" - ')
-    if key != '0':
-        item=input('введите элемент - ')
-        dictionary.update({key:item})
-    else: break
-print("создали словарь - ", dictionary)
-dictionary=dictionary.items()
-print("преоброзовали словарь в список кортежей - ", dictionary)
-dictionary=sorted(dictionary)
-print("отсортировали список кортежей по алфавиту - ", dictionary)
-dictionary1=dict(dictionary)
-print("решение 1 - словарь по алфавиту - ", dictionary1)
-dictionary2=reversed(dictionary)
-dictionary3=dict(dictionary2)
-print("решение 2 - словарь по убыванию - ", dictionary3)
+'''
+1-вводим символ из которого рисунок
+2-вводим размер рисунка (ромб, высота и ширина)
+3-цикл который печатает строку
+4-цикл который переводит строку
+'''
+s=input('Введите символ из которого будет рисунок - ')
+num=int(input('введите размер рисунка - '))
+w=int(input('введите количество узоров - '))
+for r in range(1,w):
+    print('*'*(num*2-1))
+    for i in range(1,num):
+        print('*'*(num-i)+s*i+s*(i-1)+'*'*(num-i))
+    for i in range(0,num-2):
+        print('*'*(i+2)+s*(num-i-2)+s*(num-i-3)+'*'*(i+2))
+    print('*'*(num*2-1),'\n')
